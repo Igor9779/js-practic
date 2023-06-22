043feaad88e3560471f35c7bfd113556049b439d
Igor9779 <darthhoit97@gmail.com>
2023-06-22 16:00:29 +0300

    practic-1

--- js/script.js
+++ js/script.js
@@ -194,7 +194,7 @@
 // Додайте метод "вирахувати", який обчислює середнє значення всіх чисел у масиві.
 
 // class AverageValue {
-//     constructor(numbers = []) { 
+//     constructor(numbers = []) {
 //         this.numbers = numbers;
 //     }
 
@@ -217,3 +217,124 @@
 
 // console.log(value);
 // console.log(value.average());
+
+// *******************МЕТОДИ МАСИВІВ/ОБЬЕКТІВ*****************
+
+// EXERCISE 1
+// Задача: Обернути масив
+// Опис: Напишіть функцію, яка приймає масив і повертає новий масив,
+// в якому елементи розташовані у зворотному порядку.
+
+// const fnA = function (array) {
+//     return array.reverse()
+// };
+
+// const fnA = array => array.reverse();
+
+// fnA([1, 2, 3, 4, 5]);
+// console.log(fnA([1, 2, 3, 4, 5]));
+
+
+// EXERCISE 2
+// Задача: Фільтрувати парні числа
+// Опис: Напишіть функцію, яка приймає масив чисел і повертає новий масив,
+// в якому зберігаються тільки парні числа.
+
+// const getPairedNum = numbers => [...numbers].filter(number => number % 2 === 0);
+
+// getPairedNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// console.log(getPairedNum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+
+// EXERCISE 3
+// Задача: Об'єднати масиви
+// Опис: Напишіть функцію, яка об'єднує два масиви в один.
+
+// const joinArr = (arr1, arr2) => arr1.concat(arr2);
+
+// joinArr([1, 2, 3], [4, 5, 6]);
+// console.log(joinArr([1, 2, 3], [4, 5, 6]));
+
+
+// EXERCISE 4
+// Задача: Обчислити суму чисел
+// Опис: Напишіть функцію, яка приймає масив чисел і повертає суму всіх чисел.
+
+// const getTotal = array => array.reduce((acc, number) => {
+//     return acc + number
+// }, 0);
+
+// getTotal([1, 2, 3, 4, 5]);
+// console.log(getTotal([1, 2, 3, 4, 5]));
+
+
+// EXERCISE 5
+// Задача: Видалити дублікати
+// Опис: Напишіть функцію, яка видаляє всі дублікати з масиву і повертає новий масив з унікальними елементами.
+
+// const removeDublicate = numbers => numbers.filter((number, index, array) => array.indexOf(number) === index);
+
+//     removeDublicate([1, 2, 3, 4, 3, 2, 1, 5]);
+//     console.log(removeDublicate([1, 2, 3, 4, 3, 2, 1, 5]));
+
+// EXERCISE 6
+// Задача: Отримати ключі об'єкта
+// Опис: Напишіть функцію, яка приймає об'єкт і повертає масив, що містить всі ключі цього об'єкта.
+
+// const getKeys = obj => Object.keys(obj);
+
+// getKeys({ name: 'John', age: 30, city: 'New York' });
+// console.log(getKeys({ name: 'John', age: 30, city: 'New York' }));
+
+
+// EXERCISE 7
+// Задача: Отримати значення об'єкта
+// Опис: Напишіть функцію, яка приймає об'єкт і повертає масив, що містить всі значення цього об'єкта.
+
+// const getValues = obj => Object.values(obj);
+
+// getValues({ name: 'John', age: 30, city: 'New York' });
+// console.log(getValues({ name: 'John', age: 30, city: 'New York' }));
+
+// EXERCISE 8
+// Задача: Перевірити наявність ключа
+// Опис: Напишіть функцію, яка перевіряє, чи є вказаний ключ у об'єкті.
+
+// const getHasProp = (obj, keys) => obj.hasOwnProperty(keys);
+
+// getHasProp({ name: 'John', age: 30, city: 'New York' }, 'city');
+// console.log(getHasProp({ name: 'John', age: 30, city: 'New York' }, 'city'));
+
+
+// EXERCISE 9
+// Задача: Об'єднати об'єкти
+// Опис: Напишіть функцію, яка об'єднує два об'єкти в один.
+
+// const getMergedObj = (obj1, obj2) => Object.assign(obj1, obj2);
+
+
+// getMergedObj({ name: 'John', age: 30 }, { city: 'New York', country: 'USA' });
+// console.log(getMergedObj({ name: 'John', age: 30 }, { city: 'New York', country: 'USA' }));
+
+
+// EXERCISE 10
+// Задача: Видалити властивість об'єкта
+// Опис: Напишіть функцію, яка видаляє вказану властивість з об'єкта.
+
+// const exampleObj = { name: 'John', age: 30, city: 'New York' };
+// const removeProp = (obj, keys) => delete obj[keys];
+
+// removeProp(exampleObj, 'name');
+// console.log(exampleObj);
+
+
+// EXERCISE 11
+// Задача: Конвертувати рядок в число
+// Опис: Напишіть функцію, яка приймає рядок, що представляє число, і повертає це число у числовому форматі.
+
+// const convertString = string => Number(string);
+
+// convertString('123');
+// console.log(convertString('123'));
+
+
